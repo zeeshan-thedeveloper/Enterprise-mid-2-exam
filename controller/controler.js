2ef61775c4a8bffd0195773c8b27959f0c670beb
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-05-24 15:26:40 +0500

    created a registration page

--- controller/controler.js
+++ controller/controler.js
@@ -1,10 +1,12 @@
+const {Users} =require('../model/Schemas/UserSchema')
+const path = require("path")
 // Page loaders
 const LoadHomePage=(req,res)=>{
     res.render('AddUser')
 }
 
 const LoadListOfUsersPage=(req,res)=>{
-    res.render('UsersList',{user_id:req.query.user_id})
+    res.render('UserList',{user_id:req.query.user_id})
 }
 
 // Database manipulation
@@ -13,8 +15,13 @@ const registerUser=(req,res)=>{
     const image=req.files.image
     console.log(image)
     console.log(req.body);
-    
-    res.send("Adding user")
+    image.mv(path.resolve(__dirname,'../../public/images',image.name),(error)=>{
+        Users.create({...req.body,image:image.name},(error,user)=>{
+            console.log(user)
+            res.redirect(`/UsersList?user_id=${user._id}`) 
+        })
+    });
+  
 }
 
 module.exports={
