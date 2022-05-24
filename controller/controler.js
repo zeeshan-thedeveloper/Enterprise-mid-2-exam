ac9fe5ca00b7a4cb294ac3a31bb7b552b64c8ed3
zeeshan-thedeveloper <zeeshanahmedd0010@gmail.com>
2022-05-24 16:12:46 +0500

    created page and added funtionality to update user

--- controller/controler.js
+++ controller/controler.js
@@ -1,5 +1,6 @@
 const {Users} =require('../model/Schemas/UserSchema')
 const path = require("path")
+const fs= require("fs")
 // Page loaders
 const LoadHomePage=(req,res)=>{
     res.render('AddUser')
@@ -8,6 +9,16 @@ const LoadHomePage=(req,res)=>{
 const LoadListOfUsersPage=async (req,res)=>{
     const listOfUsers = await Users.find({});
     res.render('UserList',{listOfUsers})
+}
+
+const LoadUserUpdatePage= async (req,res)=>{
+    const userData = await Users.find({_id:req.query.user_id});
+    console.log(userData)
+    res.render('UpdateUser',{userData})
+}
+
+const LoadDeleteInfoPage=(req,res)=>{
+    res.render('DeleteInfo')
 }
 
 // Database manipulation
@@ -25,8 +36,45 @@ const registerUser=(req,res)=>{
   
 }
 
+const upateUserData=(req,res)=>{
+    console.log("Updating user data")
+    console.log(req.body)
+    const image=req.files.image
+   
+image.mv(path.resolve(__dirname,'../public/images', image.name),(error)=>{
+    if(!error){
+
+        Users.findById(req.query.user_id,(error,doc)=>{
+            console.log(doc)
+            if(!error){
+                const oldImage=path.resolve(__dirname,'../public/images',doc.image)
+                fs.unlinkSync(oldImage)
+
+                Users.findByIdAndUpdate(req.query.user_id,{...req.body,image:image.name},(error, doc)=>{
+                    if(!error){
+                        res.redirect('/UsersList')
+                    }
+                    else{
+                        console.log(error)
+                        res.send("Error")
+                        // res.redirect('/api/updateItem')
+                    }
+                })
+            }
+        })
+    }
+    else{
+        res.send('Error')
+    }
+})
+
+}
+
 module.exports={
     LoadHomePage,
     LoadListOfUsersPage,
     registerUser,
+    LoadUserUpdatePage,
+    LoadDeleteInfoPage,
+    upateUserData
 }
--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,7 @@ const routes=express()
 routes.get("/Register",controller.LoadHomePage);
 routes.get("/UsersList",controller.LoadListOfUsersPage);
 routes.post("/registerUser",controller.registerUser);
-
+routes.get("/UpdateUserData",controller.LoadUserUpdatePage);
+routes.get("/DeletionInfo",controller.LoadDeleteInfoPage);
+routes.post("/upateUserData",controller.upateUserData)
 module.exports = routes;
